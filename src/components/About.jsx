5febde152f5e6b4c6ede3ce82301114adf2cfed0
prakash-200 <prakashrajrko1@gmail.com>
2024-07-14 19:20:27 +0530

    c8

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -55,10 +55,13 @@ function About() {
           </motion.div>
         </div>
         <div className="col-9 abt-para">
+          <div className="para-div">
+
           <p className='fade-in'>I'm a recent graduate with a BTech in Information Technology. Throughout my academic journey, I have developed a deep passion for coding and have acquired proficiency in web development. My hands-on experience extends to developing several projects.<br /><br />
             My enthusiasm for technology drives me to continuously learn and adapt to new advancements in the field. I enjoy exploring innovative solutions and tackling complex problems, which has fostered a strong problem-solving mindset.<br /><br />
             With a strong foundation in information technology and a relentless passion for coding, I am ready to embark on a rewarding career and make meaningful contributions to the tech industry.
           </p>
+          </div>
         </div>
       </div>
 
