9262a49b2868bc7a0224bdc4066b2e7f9c604295
prakash-200 <prakashrajrko1@gmail.com>
2024-07-17 14:44:27 +0530

    rt2

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -25,31 +25,9 @@ function Skill() {
           </Link>
         </div>
       </div>
-      <div className="row skl-footer">
-        <div className="col col-2 skl-link align-self-end">
-          <motion.div className="col" initial={{ y: 300 }}
-            animate={{ y: 0 }}
-            transition={{ type: 'spring', stiffness: 100 }}>
-            <motion.button className='border border-0 bg-transparent fs-2'
-              animate={{ scale: [1, 1.5, 1] }}
-              transition={{ delay: 1.0, duration: 1, ease: "easeInOut" }}
-            >
-              <a href="https://www.linkedin.com/in/prakashraj200/" className='text-dark px-4' target='_blank'><FaLinkedin className='scale-on-hover' /></a>
-            </motion.button><br />
-            <motion.button className='border border-0 bg-transparent fs-2'
-              animate={{ scale: [1, 1.5, 1] }}
-              transition={{ delay: 2.0, duration: 1, ease: "easeInOut" }}
-            >
-              <a href="https://github.com/prakash-200" className='text-dark px-4' target='_blank'><FaGithub className='abt-arrow' /></a>
-            </motion.button><br />
-            <motion.button className='border border-0 bg-transparent fs-2'
-              animate={{ scale: [1, 1.5, 1] }}
-              transition={{ delay: 3.0, duration: 1, ease: "easeInOut" }}
-            >
-              <a href="https://www.instagram.com/coding__luv/" className='text-dark px-4' target='_blank' rel="noopener noreferrer"><FaInstagramSquare className='abt-arrow' /></a>
-            </motion.button>
-            <p className='db-vertical px-3 text-dark'>______________________</p>
-          </motion.div>
+      <div className="row skl-content">
+        <div className="col col-1 col-md-1 skl-link align-self-end">
+          
         </div>
         <div className="col col-10 skl-card">
           <motion.div className="c1" initial={{ y: 900 }}
@@ -79,6 +57,31 @@ function Skill() {
           </motion.div>
           
         </div>
+      </div>
+      <div className="row skl-footer">
+      <motion.div className="col" initial={{ y: 300 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 100 }}>
+            <motion.button className='border border-0 bg-transparent fs-2'
+              animate={{ scale: [1, 1.5, 1] }}
+              transition={{ delay: 1.0, duration: 1, ease: "easeInOut" }}
+            >
+              <a href="https://www.linkedin.com/in/prakashraj200/" className='text-dark px-4' target='_blank'><FaLinkedin className='scale-on-hover' /></a>
+            </motion.button><br />
+            <motion.button className='border border-0 bg-transparent fs-2'
+              animate={{ scale: [1, 1.5, 1] }}
+              transition={{ delay: 2.0, duration: 1, ease: "easeInOut" }}
+            >
+              <a href="https://github.com/prakash-200" className='text-dark px-4' target='_blank'><FaGithub className='abt-arrow' /></a>
+            </motion.button><br />
+            <motion.button className='border border-0 bg-transparent fs-2'
+              animate={{ scale: [1, 1.5, 1] }}
+              transition={{ delay: 3.0, duration: 1, ease: "easeInOut" }}
+            >
+              <a href="https://www.instagram.com/coding__luv/" className='text-dark px-4' target='_blank' rel="noopener noreferrer"><FaInstagramSquare className='abt-arrow' /></a>
+            </motion.button>
+            <p className='db-vertical px-3 text-dark'>______________________</p>
+          </motion.div>
       </div>
     </div>
   )
