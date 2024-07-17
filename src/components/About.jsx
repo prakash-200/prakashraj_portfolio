f876b4b09a99b7e7be481e0360f16d36f6fa2b46
prakash-200 <prakashrajrko1@gmail.com>
2024-07-17 10:09:46 +0530

    rt1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
   return (
     <div className="abt d-flex flex-column">
 
-      <div className="row abt-header bg-primary">
+      <div className="row abt-header">
         <div className='col'>
           <p className="text-light abt-name p-3">JP</p>
         </div>
@@ -24,16 +24,43 @@ function About() {
         </div>
       </div>
 
-      <div className="row abt-content bg-warning">
+      <div className="row abt-content">
         <p className='abt-about'>ABOUT</p>
       </div>
 
-<div className="row abt-footer bg-secondary">
-  <div className="col col-2 bg-light"></div>
-  <div className="col col-10 bg-dark">
-    <p className='text-info para-div'>I'm a recent graduate with a BTech in Information Technology. Throughout my academic journey, I have developed a deep passion for coding and have acquired proficiency in web development. My hands-on experience extends to developing several projects.<br /><br />
+<div className="row abt-footer">
+  <div className="col col-2 align-self-end">
+  <motion.div className="col" initial={{ y: 300 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 100 }}>
+            <motion.button className='border border-0 bg-transparent fs-2'
+              animate={{ scale: [1, 1.5, 1] }}
+              transition={{ delay: 1.0, duration: 1, ease: "easeInOut" }}
+            >
+              <a href="https://www.linkedin.com/in/prakashraj200/" className='text-light px-4' target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
+            </motion.button><br />
+            <motion.button className='border border-0 bg-transparent fs-2'
+              animate={{ scale: [1, 1.5, 1] }}
+              transition={{ delay: 2.0, duration: 1, ease: "easeInOut" }}
+            >
+              <a href="https://github.com/prakash-200" className='text-light px-4' target='_blank' rel="noopener noreferrer"><FaGithub/></a>
+            </motion.button><br />
+            <motion.button className='border border-0 bg-transparent fs-2'
+              animate={{ scale: [1, 1.5, 1] }}
+              transition={{ delay: 3.0, duration: 1, ease: "easeInOut" }}
+            >
+              <a href="https://www.instagram.com/coding__luv/" className='text-light px-4' target='_blank' rel="noopener noreferrer"><FaInstagramSquare /></a>
+            </motion.button>
+            <p className='db-vertical px-3 text-light'>______________________</p>
+          </motion.div>
+  </div>
+  <div className="col col-10">
+    <div className="">
+
+    <p className='text-light para-div fade-in'>I'm a recent graduate with a BTech in Information Technology. Throughout my academic journey, I have developed a deep passion for coding and have acquired proficiency in web development. My hands-on experience extends to developing several projects.<br /><br />
             My enthusiasm for technology drives me to continuously learn and adapt to new advancements in the field. I enjoy exploring innovative solutions and tackling complex problems, which has fostered a strong problem-solving mindset.<br /><br />
             With a strong foundation in information technology and a relentless passion for coding, I am ready to embark on a rewarding career and make meaningful contributions to the tech industry.</p>
+    </div>
   </div>
 </div>
       {/* <div className="row d-flex abt-footer bg-secondary">
